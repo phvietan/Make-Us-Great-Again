4383b83d71273fb8ce634c91a46ae6e91d28576f
silver <phvietan@gmail.com>
2019-06-01 22:32:20 +0700

    update 1 June 2019

---
+++ Original-Caesar-Cipher/run.py
@@ -0,0 +1,31 @@
+def shift(i):
+  val = ord(i)
+  A = ord('A')
+  Z = ord('Z')
+  if (val >= A and val <= Z):
+    val = val + 1
+    if (val > Z): val = A
+    return chr(val)
+
+  a = ord('a')
+  z = ord('z')
+  if (val >= a and val <= z):
+    val = val + 1
+    if (val > z): val = a
+    return chr(val)
+
+  return i
+
+def shiftString(s):
+  result = ''
+  for i in range(len(s)):
+    result += shift(s[i])
+  return result
+
+f = open('input.txt', 'r')
+
+content = f.read()
+
+for i in range(26):
+  print(content)
+  content = shiftString(content)
