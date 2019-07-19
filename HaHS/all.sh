7648e3a1808246d56aa09eee44acb62f3e90093b
HaRiSK <huynhsamha@gmail.com>
2019-07-19 13:07:49 +0700

    add template

---
+++ HaHS/all.sh
@@ -0,0 +1,5 @@
+# Build main.cpp to binary file
+make
+
+# Run binary file ./main
+./main
---
+++ HaHS/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef unsigned long long ull;
+
+#define X first
+#define Y second
+#define pb push_back
+#define EL printf("\n")
+#define sz(A) (int)A.size()
+#define FOR(i, l, r) for (int i = l; i <= r; i++)
+#define FOD(i, r, l) for (int i = r; i >= l; i--)
+#define faster                                                                 \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(NULL);                                                             \
+    cout.tie(NULL);
+
+int main() { return 0; }
